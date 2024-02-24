a68be833b394fefc9fd0bd793fd8d452e6881bd5
Ikigai08 <114841248+Ikigai08@users.noreply.github.com>
2024-02-24 16:15:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    var resumeContainer = document.querySelector('.resume-container');
+    resumeContainer.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
